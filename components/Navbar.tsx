4a0f664c7fe25b1a5014de026c8700c0dc153a38
Aman Kumar <akr170802@gmail.com>
2026-03-11 10:22:08 +0530

    updated navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,16 @@
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center px-4 md:px-8 py-4 bg-white shadow">
+    <nav className="flex justify-between items-center px-4 md:px-8 py-4 bg-red-600 shadow">
 
-      <h1 className="text-2xl font-bold text-red-600">
+      <h1 className="text-2xl font-bold text-white">
         WBlu
       </h1>
 
       <div className="flex gap-3 md:gap-6 text-gray-700 text-sm md:text-base">
-        <a href="/" className="hover:text-red-600">Home</a>
-        <a href="#products" className="hover:text-red-600">Products</a>
-        <a href="#features" className="hover:text-red-600">About</a>
-        <a href="#contact" className="hover:text-red-600">Contact</a>
+        <a href="/" className="text-white hover:text-red-900">Home</a>
+        <a href="#products" className="text-white hover:text-red-900">Products</a>
+        <a href="#features" className="text-white hover:text-red-900">About</a>
+        <a href="#contact" className="text-white hover:text-red-900">Contact</a>
       </div>
 
     </nav>
